aa6f8e171673d6a07d3e58ea6ba68942f1212aa3
Talu <talu.whitley@gmail.com>
2026-07-29 10:01:34 -0600

    gone

--- Automation/Python/Netmiko/inventory.py
+++
@@ -1,5 +0,0 @@
-DEVICES = [
-    {"host" : "devnetsandboxiosxec8k.cisco.com"},
-    {"host": "devnetsandboxiosxec9k.cisco.com"},
-    {"host": "10.0.0.5"}
-    ]
--- Automation/Python/Netmiko/my_creds.py
+++
@@ -1,6 +0,0 @@
-my_device ={
-    "device_type" : "cisco_ios",
-    "host" : "devnetsandboxiosxec9k.cisco.com",
-    "username" : "talu.whitley",
-    "password" : "0rZeKm5N0b6--xVy"
-}
