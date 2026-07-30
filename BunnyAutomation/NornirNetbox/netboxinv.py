8943b4308308a0539e83cbe7176ca84694c18a0b
Talu <talu.whitley@gmail.com>
2026-07-30 11:13:02 -0600

    Added instructions

--- BunnyAutomation/NornirNetbox/netboxinv.py
+++ BunnyAutomation/NornirNetbox/netboxinv.py
@@ -1,12 +1,14 @@
+'''Basic Nornir job that goes through Netbox as your inventory including filter parameters'''
+
 from nornir import InitNornir
 from rich import print as rprint
 
-nr = InitNornir(config_file="config.yaml")
+nr = InitNornir(config_file="config.yaml") #Initialize Nornir
 
 def randomtest(task):
-    rprint(f"Hell, my name is {task.host}")
-    rprint(f"My Ip Address/Hostname is {task.host.hostname}")
-    rprint(f"My platform is {task.host.platform}")
+    rprint(f"Hell, my name is {task.host}") #Print Host/Name from Netbox of object
+    rprint(f"My Ip Address/Hostname is {task.host.hostname}") #Print IP or Hostname within field of Object
+    rprint(f"My platform is {task.host.platform}") #Print Platform field of object
 
 
 
